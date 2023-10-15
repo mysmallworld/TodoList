ca253b4154dd81f8ca6841c851927ed1b861b0cc
Inès C <ines.parisdescartes@gmail.com>
2023-10-15 18:05:35 +0200

    add connexion BDD

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser').json();
 const app = express();
-const mysql = require('mysql2');
-const connexion = require('express-myconnection');
 const port = 8080;
 
-const bdd = {
+// Partie connection à la BDD avec mysql
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
     port: 3306,
     database: "projettodolist",
-};
+});
 
-app.use(connexion(mysql, bdd, 'pool'));
+connection.connect((err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log("Vous êtes connecté à la BDD !");
+})
 
+// Permet d'interpréter le body JSON de notre réponse HTTP
 app.use(bodyParser);
+
+// Variable de nos requêtes 
+let sql = '';
 
 // Variable globale pour notre liste de tâches
 let listTask = [
@@ -47,59 +57,103 @@ let listTask = [
 
 // Afficher toutes les tâches
 app.get('/', (req, res) => {
-    res.json(listTask);
+    connection.query('SELECT * FROM tasks', (err, rows, fields) => {
+        if (err) throw err;
+        res.json(rows);
+    });
 })
 
 // Afficher uniquement les tâches non faites
 app.get('/undone', (req, res) => {
-    let tab = [];
-    for (let i = 0; i < listTask.length; i++) {
-        if (listTask[i].isDone === false) {
-            tab.push(listTask[i]);
+    sql = 'SELECT * FROM tasks';
+
+    connection.query(sql, (err, rows, fields) => {
+        let tab = [];
+        for (let i = 0; i < rows.length; i++) {
+            if (rows[i].isDone === 0) {
+                tab.push(rows[i]);
+            }
         }
-    }
-    console.log(`Il vous reste ${tab.length} tâches à terminer !`);
-    return res.json(tab);
+
+        if (err) throw err;
+        
+        tab.length < 2 ? console.log(`Il vous reste ${tab.length} tâche à faire !`) : console.log(`Il vous reste ${tab.length} tâches à faire !`);
+        
+        return res.json(tab);
+    });
 })
 
 // Ajouter une tâche
-app.post('/edit', (req, res) => {
-    listTask.push(req.body);
-    res.json(listTask);
+app.post('/', (req, res) => {
+    sql = "INSERT INTO tasks(id, name, dueDate, isDone) VALUES(?,?,?,?)";
+    let values = [req.body.id, req.body.name, req.body.dueDate, req.body.isDone];
+
+    connection.query(sql, values, (err, rows, fields) => {
+        let tab = [];
+        if (err) throw err;
+        tab.push(req.body);
+        res.json(tab);
+    });
 })
 
-// Marquer une tâche à done
+// Marquer une tâche à faite
 app.put('/done/:id', (req, res) => {
-    const task = listTask.find(todo => todo.id == req.params.id)
-    if (task === undefined) {
-        console.log(err);
-    }
-    task.isDone = true
-    console.log(`Bravo, votre tâche ${task.name} est terminée !`);
-    res.json(task);
+    sql = "UPDATE tasks SET id = ?, name = ?, dueDate = ?, isDone = ? WHERE id = ?";
+    let values = [req.body.id, req.body.name, req.body.dueDate, req.body.isDone, req.params.id];
+
+    connection.query(sql, values, (err, rows, fields) => {
+        if (err) throw err;
+
+        const task = listTask.find(todo => todo.id == req.params.id)
+        if (task === undefined) {
+            console.log(err);
+        }
+
+        task.isDone = true
+        console.log(`Bravo, votre tâche ${task.name} est terminée !`);
+        res.json(task);
+    });
 })
 
 // Modifier une tâche existante
 app.put('/:id', (req, res) => {
-    const task = listTask.find(todo => todo.id == req.params.id)
-    if (task === undefined) {
-        console.log(err);
-    }
-    task.name = req.body.name;
-    task.dueDate = req.body.dueDate;
-    task.isDone = req.body.isDone;
-    console.log(`Votre tâche a bien été modifiée !`);
-    res.json(task);
+    sql = "UPDATE tasks SET id = ?, name = ?, dueDate = ?, isDone = ? WHERE id = ?";
+    let values = [req.body.id, req.body.name, req.body.dueDate, req.body.isDone, req.params.id];
+
+    connection.query(sql, values, (err, rows, fields) => {
+        if (err) throw err;
+
+        const task = listTask.find(todo => todo.id == req.params.id)
+
+        if (task === undefined) {
+            console.log(err);
+        }
+
+        task.name = req.body.name;
+        task.dueDate = req.body.dueDate;
+        task.isDone = req.body.isDone;
+        console.log('Votre tâche a bien été modifiée !');
+        res.json(task);
+    })
 })
 
 // Supprimer une tâche
 app.delete('/:id', (req, res) => {
-    const taskIndex = listTask.findIndex(t => t.id == req.params.id);
-    listTask.splice(taskIndex, 1);
-    console.log(`Votre tâche a bien été supprimée !`);
-    return res.json(listTask);
+    sql = "DELETE FROM tasks WHERE id = ?";
+    let values = [req.params.id];
+
+    connection.query(sql, values, (err, rows, fields) => {
+        if (err) throw err;
+
+        const taskIndex = listTask.findIndex(t => t.id == req.params.id);
+        listTask.splice(taskIndex, 1);
+
+        console.log('Votre tâche a bien été supprimée !');
+        res.json(listTask);
+    })
 })
 
+// Port que l'on veut écouter
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 })
